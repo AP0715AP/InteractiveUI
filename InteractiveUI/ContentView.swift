4d2a74648b95c00281d0899daedad89134dae22b
Akshara <sweetakshara.0715@gmail.com>
2025-07-23 13:42:34 -0700

    Added Text Field + Button

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name: String = ""
+
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             Text("What is your name?")
                 .font(.title)
-            
+
+            TextField("Type your name here...", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+
+            Button("Submit Name") {
+                print("Name submitted: \(name)")
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
         }
         .padding()
     }
@@ -21,3 +34,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
